38c5fc1dbd90fc932d479581c719a60c26d1b353
rg98098 <rg98098@gmail.com>
2021-12-01 19:03:57 +0530

    few fixes of styling, added Header ScreenHeading reusable component

---
+++ src/components/AboutMe/AboutMe.js
@@ -0,0 +1,14 @@
+import React, {useEffect} from 'react'
+import { ScreenHeading } from '../../utilities/ScreenHeading/ScreenHeading'
+import ScrollService from '../../utilities/ScrollService'
+import Animations from '../../utilities/Animations'
+
+export const AboutMe = (props) => {
+  return (
+    <div className="about-me-container screen-container">
+      <div className="about-me-parent">
+        <ScreenHeading title={'About Me'} subHeading={'Why Choose Me'} />
+      </div>
+    </div>
+  )
+}
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,10 +2,12 @@ import React from 'react'
 import Footer from './Footer/Footer'
 import { Profile } from './Profile/Profile'
 import './Home.scss'
+import { Header } from './Header/Header'
 
 export const Home = () => {
   return (
     <div className="home-container">
+      <Header/>
       <Profile/>
       <Footer/>
     </div>
